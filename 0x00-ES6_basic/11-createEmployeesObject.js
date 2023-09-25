8bebf4a41f60f7945dae160b5c07c9886ede52af
Melaku Birhanu <melakubirhanu@gmail.com>
2023-09-25 19:31:15 +0000

    ES6

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,10 @@
+export default function createEmployeesObject(departmentName, employees) {
+
+	let x = employees;
+	let y = departmentName;
+	let node;
+	for (x of y) {
+		node.ppend(x);
+	}
+	return node;
+}
